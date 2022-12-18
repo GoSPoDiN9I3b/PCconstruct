5d96dade95ee5e66d63192918fd8d74a6dda40d4
GoSPoDiN9I3b <120991572+GoSPoDiN9I3b@users.noreply.github.com>
2022-12-19 01:40:11 +0300

    Add files via upload

---
+++ blocks/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+	<title>Интернет магазин</title>
+</head>
+<body>
+	<div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+       <img class="logo" src="img/logo.jpg" >
+        <span class="fs-4">Мой Компьютер - магазин комплектующих для вашего ПК</span>
+      </a>
+
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-dark text-decoration-none" href="index.php">Главная</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="construkt.php">ПК конструктор</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="about.php">Связаться с нами</a>
+        <a class="py-2 text-dark text-decoration-none" href="reg.php">Войти</a>
+
+      </nav>
+    </div>
+    </body>
+</html>
